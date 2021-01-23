8cdfc78b9ec1cc1d35b6e1fcd8d20954f8fbc136
jordan-medium <jordanjwilliams.medium@gmail.com>
2021-01-23 14:02:48 -0500

    Full code

---
+++ src/terminal/AnotherClass.java
@@ -0,0 +1,10 @@
+package terminal;
+/**
+ * AnotherClass
+ */
+public class AnotherClass {
+
+    public void runningSecondClass() {
+        System.out.println("Running second class in terminal");
+    }
+}
---
+++ src/terminal/DemoTerminal.java
@@ -0,0 +1,13 @@
+package terminal;
+/**
+ * DemoTerminal
+ */
+public class DemoTerminal {
+
+    public static void main(String[] args) {
+        AnotherClass anotherClass = new AnotherClass();
+
+        System.out.println("Running in terminal....");
+        anotherClass.runningSecondClass();
+    }
+}
